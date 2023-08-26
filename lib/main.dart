ca00ef1b49ff398a9ef7e583dd03fa70f4b95986
azzouzin <78663404+azzouzin@users.noreply.github.com>
2023-08-26 19:44:28 +0100

    tesr

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       darkTheme: darkTheme,
       //  home: Example(title: 'HOHO'),
       //home: MapScreen(),
-      initialRoute: '/',
+      initialRoute: '/signup',
       getPages: [
         GetPage(name: '/', page: () => const OneBoard()),
         GetPage(name: '/signup', page: () => const SignupPage()),
